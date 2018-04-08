8ddd033e6ea999bbc2e934d027eaf944358ddf86
Christopher Di Bella <cjdb.ns@gmail.com>
2018-04-08 15:42:26 +0100

    Added some auto-detect features for CMake.

--- examples/lighting/spotlight/example.spot_light.frag.glsl
+++ examples/lighting/spotlight/example.spot_light.frag.glsl
@@ -41,26 +41,26 @@ void main()
    const float theta = dot(light_dir, normalize(-light.direction));
    const float epsilon = light.inner_cutoff - light.outer_cutoff;
    const float intensity = clamp((theta - light.outer_cutoff) / epsilon, 0.0, 1.0);
-      // ambient
-      const vec3 ambient = light.ambience * texture(material.diffuse, frag_texture_coordinates).rgb;
+   // ambient
+   const vec3 ambient = light.ambience * texture(material.diffuse, frag_texture_coordinates).rgb;
 
-      // diffuse
-      const vec3 norm = normalize(frag_normal);
-      const float diff = max(dot(norm, light_dir), 0.0);
-      vec3 diffuse = light.diffuse * diff * texture(material.diffuse, frag_texture_coordinates).rgb;
+   // diffuse
+   const vec3 norm = normalize(frag_normal);
+   const float diff = max(dot(norm, light_dir), 0.0);
+   vec3 diffuse = light.diffuse * diff * texture(material.diffuse, frag_texture_coordinates).rgb;
 
-      // specular
-      const vec3 view_dir = normalize(view_position - frag_position);
-      const vec3 reflect_dir = reflect(-light_dir, norm);
-      const float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
-      vec3 specular = light.specular * spec * texture(material.specular, frag_texture_coordinates).rgb;
+   // specular
+   const vec3 view_dir = normalize(view_position - frag_position);
+   const vec3 reflect_dir = reflect(-light_dir, norm);
+   const float spec = pow(max(dot(view_dir, reflect_dir), 0.0), material.shininess);
+   vec3 specular = light.specular * spec * texture(material.specular, frag_texture_coordinates).rgb;
 
-      // attenuation
-      const float distance = length(light.position - frag_position);
-      const float attenuation = 1.0 / (light.attenuation.constant + light.attenuation.linear * distance
-         + light.attenuation.quadratic * (distance * distance));
+   // attenuation
+   const float distance = length(light.position - frag_position);
+   const float attenuation = 1.0 / (light.attenuation.constant + light.attenuation.linear * distance
+      + light.attenuation.quadratic * (distance * distance));
 
-      diffuse *= attenuation * intensity;
-      specular *= attenuation * intensity;
-      frag_colour = vec4(ambient + diffuse + specular, 1.0);
+   diffuse *= attenuation * intensity;
+   specular *= attenuation * intensity;
+   frag_colour = vec4(ambient + diffuse + specular, 1.0);
 }
